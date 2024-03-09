4cc7d3bae6d7d037b4e35a123dc914c2f55844eb
Timur_JS <worktim915@gmail.com>
2024-03-09 13:10:14 +0200

    our first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+// class
+
+class Animal {
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    // методи
+    introduce() {
+        console.log(`Привіт! Мене звати ${this.name}. Я - ${this.type}`);
+    }
+}
+
+const dog = new Animal("Бетховен", "собака");
+dog.introduce();
+
+const cat = new Animal("Гарфілд", "кіт");
+cat.introduce();
+//
+
+const fish = new Animal("Немо", 'риба');
+fish.introduce();
+
+
+
+
+
+// new Class
+
+
+
+
+// 
+// Class 
+// Ми робимо описа класу. Цей клас зазначає розміри тварин.
+
+class AnimalSize extends Animal {
+    constructor(name, type, size) {
+        super(name, type, size)
+        this.size = size;
+    }
+
+    introduce() {
+        console.log(`Привіт! Мене звати ${this.name}. Я - ${this.type} i важу ${this.size} кг`);
+    }
+}
+
+const dogSize = new AnimalSize("Бетховен", "собака", "15");
+console.log(dogSize);
+dogSize.introduce();
